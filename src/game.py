3a56272456bb0941903f6258deed9b70105e1f34
elim5846 <eric.lim5846@gmail.com>
2023-06-16 12:08:40 +0200

    cleaned removed the optimization

--- src/game.py
+++ src/game.py
@@ -12,75 +12,58 @@ class Game:
         time: int,
         index: int,
         records: dict,
-        immune: bool,
-        counter: int,
+        max_geodes: int,
     ) -> None:
         self.player = player
         self.time = time
         self.index = index
         self.records = records
-        self.immune = immune
-        self.counter = counter
+        self.max_geodes = max_geodes
 
     def start(self):
         max_geode = self.player.materials.get(Type.GEODE, 0)
-        """
-        code pas fou ici mon on essaie deja de faire marcher le tout
-        """
-        records2 = self.records[self.index]
-
-        # if (all([records2[key] is not None for key in records2])) and (
-        #     all([self.index > records2[key] for key in records2])
-        #     and not (self.immune)
-        # ):
-        #     return max_geode
-
-        records3 = {key: value for key, value in records2.items() if value is not None}
-        for key in records3:
-            if self.player.materials.get(key) is None:
-                print(self.player.materials)
-                print(self.index)
-                return max_geode
 
         for i in range(self.index, self.time):
-            clone = deepcopy(self.player)
+            self.index = i
             actions = self.player.get_actions_could_do()
-            for action in actions:
-                if not (action):
-                    # self.immune = True
-                    self.player.play(receipt_to_use=action)
-                    max_geode = max(max_geode, self.player.materials.get(Type.GEODE, 0))
-                    for key in self.player.materials:
-                        records = self.records[self.index]
-                        if records[key] is None:
-                            records[key] = self.index
-                            # self.immune = True
-                        elif self.index < records[key]:
-                            records[key] = self.index
-                            self.immune = True
-                    self.counter += 1
-                else:
-                    # self.immune = True
-                    clone_player = deepcopy(clone)
-                    clone_player.play(receipt_to_use=action)
-                    for key in clone_player.materials:
-                        records = self.records[self.index]
-                        if records[key] is None:
-                            records[key] = self.index
-                            # self.immune = True
-                        elif self.index < records[key]:
-                            records[key] = self.index
-                            self.immune = True
-                    new_game = Game(
-                        player=clone_player,
-                        time=self.time,
-                        index=i + 1,
-                        records=self.records,
-                        immune=self.immune,
-                        counter=0,
-                    )
-                    max_geode = max(max_geode, new_game.start())
+            self.__process_actions(actions=actions)
         return max_geode
+    
+    def __process_actions(self, actions: list) -> None:
+        for action in actions:
+            actual_player_state = deepcopy(self.player)
+            if not(action):
+                self.__play_no_action()
+            else:
+                self.__play_action(action=action, actual_player=actual_player_state)
+    
+    def __save_to_records(self, materials: dict):
+        for key in materials:
+            records = self.records[self.index]
+            if records[key] is None or self.index < records[key]:
+                records[key] = self.index
+
+    def __play_no_action(self):
+        self.player.play(receipt_to_use=None)
+        max_geode = max(self.max_geodes, self.player.materials.get(Type.GEODE, 0))
+        self.__save_to_records(materials=self.player.materials)
+
+    def __play_action(self, action: Receipt, actual_player: Player):
+        clone_player = deepcopy(actual_player)
+        clone_player.play(receipt_to_use=action)
+        self.__save_to_records(materials=clone_player.materials)
+        self.__create_new_game_and_get_max_geodes(new_player=clone_player)
+
+    def __create_new_game_and_get_max_geodes(self, new_player: Player) -> int:
+        new_game = Game(
+            player=new_player,
+            time=self.time,
+            index=self.index + 1,
+            records=self.records,
+            max_geodes=self.max_geodes
+        )
+        return max(self.max_geodes, new_game.start())
+
 
 
 limits = {
@@ -123,8 +106,7 @@ game = Game(
         }
     ]
     * 25,
-    immune=False,
-    counter=0,
+    max_geodes=0
 )
 print(game.start())
 end = time.time() - start
