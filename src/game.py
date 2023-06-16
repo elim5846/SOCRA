34180e3416dca3ac6c4424b8bf51917e676fb8ac
elim5846 <eric.lim5846@gmail.com>
2023-06-16 11:14:17 +0200

    lint

--- src/game.py
+++ src/game.py
@@ -24,45 +24,52 @@ class Game:
 
     def start(self):
         max_geode = self.player.materials.get(Type.GEODE, 0)
-        # records = {key: value for key, value in self.records.items() if value is not None}
-        # for key in records:
-        #     if (self.player.materials.get(key) is None) or (self.index > records[key] and not(self.immune)):
-        #         return max_geode
-
         """
         code pas fou ici mon on essaie deja de faire marcher le tout
         """
+        records2 = self.records[self.index]
 
-        if (all([self.records[key] is not None for key in self.records])) and (
-            all([self.index > self.records[key] for key in self.records])
-            and not (self.immune)
-        ):
-            return max_geode
+        # if (all([records2[key] is not None for key in records2])) and (
+        #     all([self.index > records2[key] for key in records2])
+        #     and not (self.immune)
+        # ):
+        #     return max_geode
+
+        records3 = {key: value for key, value in records2.items() if value is not None}
+        for key in records3:
+            if self.player.materials.get(key) is None:
+                print(self.player.materials)
+                print(self.index)
+                return max_geode
 
         for i in range(self.index, self.time):
             clone = deepcopy(self.player)
             actions = self.player.get_actions_could_do()
             for action in actions:
                 if not (action):
+                    # self.immune = True
                     self.player.play(receipt_to_use=action)
                     max_geode = max(max_geode, self.player.materials.get(Type.GEODE, 0))
                     for key in self.player.materials:
-                        if self.records[key] is None:
-                            self.records[key] = self.index
+                        records = self.records[self.index]
+                        if records[key] is None:
+                            records[key] = self.index
                             # self.immune = True
-                        elif self.index < self.records[key]:
-                            self.records[key] = self.index
+                        elif self.index < records[key]:
+                            records[key] = self.index
                             self.immune = True
                     self.counter += 1
                 else:
+                    # self.immune = True
                     clone_player = deepcopy(clone)
                     clone_player.play(receipt_to_use=action)
                     for key in clone_player.materials:
-                        if self.records[key] is None:
-                            self.records[key] = self.index
+                        records = self.records[self.index]
+                        if records[key] is None:
+                            records[key] = self.index
                             # self.immune = True
-                        elif self.index < self.records[key]:
-                            self.records[key] = self.index
+                        elif self.index < records[key]:
+                            records[key] = self.index
                             self.immune = True
                     new_game = Game(
                         player=clone_player,
@@ -107,12 +114,15 @@ game = Game(
     player=player,
     time=24,
     index=0,
-    records={
-        Type.ORE: None,
-        Type.CLAY: None,
-        Type.OBSIDIAN: None,
-        Type.GEODE: None,
-    },
+    records=[
+        {
+            Type.ORE: None,
+            Type.CLAY: None,
+            Type.OBSIDIAN: None,
+            Type.GEODE: None,
+        }
+    ]
+    * 25,
     immune=False,
     counter=0,
 )
